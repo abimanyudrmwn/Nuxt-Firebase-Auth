34a1d9c04e432bfd23f3aa2e2db7d02505106b4a
Abimanyu Darmawan <abimanyudarmawan4@gmail.com>
2022-07-02 02:20:42 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-column align-start align-self-start justify-start">
+    <h1>Welcome to dashboard</h1>
+    <p>You are now logged in as {{ $nuxt.$fire.auth.currentUser.email }}</p>
+    <v-btn
+      @click="$router.push('/auth/signout')"
+      >
+      Logout
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
